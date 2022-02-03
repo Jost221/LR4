262dafed83293a5236f5508471c1d01750cf652e
Jost221 <73162320+Jost221@users.noreply.github.com>
2022-02-03 12:51:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-
+dev input_patch():#Хренычев Максим
+  pass#need to take patch from user, check directory for exist and return patch
+dev scan_file():#Скитихин Георгий
+  pass#need to scan patch on files with processing all exist and return dictionary
+dev sort():#Шешин Александр
+  pass#sort all dictionary and return list with file dublikate
+dev printed():#тимлид
+  pass
