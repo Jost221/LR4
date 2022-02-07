d72b05236eef20fde1763134b9c339fdd8bddbce
s1uad-dw <s1uad.dw@gmail.com>
2022-02-07 11:44:37 +0300

    scan file function is ready

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import os
 def input_patch():#Хренычев Максим
   pass#need to take patch from user, check directory for exist and return patch
 
-def create_dictionary(path, level = 1, dictionary = {}):#Скитихин Георгий
+def scan_file(path, level = 1, dictionary = {}):#Скитихин Георгий
   for i in os.listdir(path):
     print(os.path.join(path, i))
     try:
       if os.path.isdir(os.path.join(path, i)):
-        create_dictionary(os.path.join(path, i), level + 1 ,dictionary)
+        scan_file(os.path.join(path, i), level + 1 ,dictionary)
       else:
         dictionary[os.path.join(path, i)] = os.stat(os.path.join(path, i)).st_size
     except Exception as error:
